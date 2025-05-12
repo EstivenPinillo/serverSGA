cf4863a6e694617b25d6bbfcad09bf3ee741b7b9
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-05-11 23:12:31 -0500

    fix: error cors

--- index.php
+++ index.php
@@ -13,5 +13,10 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=utf-8");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 $response = Route::passRequestToController();
 echo json_encode($response);
